7496eb949ed854268d7b97a5f3e478f57f60ae08
cAttte <26514199+cAttte@users.noreply.github.com>
2020-09-13 02:30:47 -0300

    feat(themes): add Coy

---
+++ src/themes/coy.js
@@ -0,0 +1,49 @@
+/**
+ * Last updated: 2020/09/12
+ * Source: https://github.com/PrismJS/prism/blob/master/themes/prism-coy.css
+ *
+ * prism.js Coy theme for JavaScript, CoffeeScript, CSS and HTML
+ * Based on https://github.com/tshedor/workshop-wp-theme
+ * @author Tim Shedor
+ */
+
+module.exports = {
+    background: "#fdfdfd",
+    color: "#000000",
+    tokens: {
+        comment: "#7D8B99",
+        "block-comment": "#7D8B99",
+        prolog: "#7D8B99",
+        doctype: "#7D8B99",
+        cdata: "#7D8B99",
+        punctuation: "#5F6364",
+        property: "#c92c2c",
+        tag: "#c92c2c",
+        boolean: "#c92c2c",
+        number: "#c92c2c",
+        "function-name": "#c92c2c",
+        constant: "#c92c2c",
+        symbol: "#c92c2c",
+        deleted: "#c92c2c",
+        selector: "#2f9c0a",
+        "attr-name": "#2f9c0a",
+        string: "#2f9c0a",
+        char: "#2f9c0a",
+        function: "#2f9c0a",
+        builtin: "#2f9c0a",
+        inserted: "#2f9c0a",
+        operator: "#a67f59",
+        entity: "#a67f59",
+        url: "#a67f59",
+        variable: "#a67f59",
+        atrule: "#1990b8",
+        "attr-value": "#1990b8",
+        keyword: "#1990b8",
+        "class-name": "#1990b8",
+        regex: "#e90",
+        important: "#e90",
+        bold: { bold: true },
+        italic: { italic: true },
+        namespace: "#000000b3"
+    }
+}
