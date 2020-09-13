b1baed66955a4d42f3e99e8faf13275db6239238
cAttte <26514199+cAttte@users.noreply.github.com>
2020-09-12 23:36:58 -0300

    feat: implement loadLanguage() and loadAllLanguages()

--- src/Prism.js
+++ src/Prism.js
@@ -1,3 +1,15 @@
-module.exports = require("prismjs")
+const Prism = require("prismjs")
+module.exports = Prism
 
-// TODO: add methods to load languages/themes, expose them in index.js
+Prism.loadLanguage = function loadLanguage(name) {
+    require(`prismjs/components/prism-${name}.min.js`)
+}
+
+Prism.loadAllLanguages = function loadAllLanguages() {
+    const languages = require("prismjs/components.json").languages
+    Object.keys(languages).forEach(language => {
+        try {
+            require(`prismjs/components/prism-${language}.min.js`)
+        } catch {}
+    })
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+const Prism = require("./Prism")
+
 module.exports.Code = require("./components/Code")
+module.exports.loadLanguage = Prism.loadLanguage.bind(Prism)
+module.exports.loadAllLanguages = Prism.loadAllLanguages.bind(Prism)
